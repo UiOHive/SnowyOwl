8be3370466ce08607110ea75decf435f9ded3d37
Luc <luc.girod@gmai.com>
2021-03-09 16:07:43 +0100

    Fixed rot matrix

--- app/snowyowl/SnowyOwl.py
+++ app/snowyowl/SnowyOwl.py
@@ -27,8 +27,8 @@ class SnowyOwl():
         Mom = np.matrix([[1, 0, 0], [0, cos(radians(extrinsic[3])), sin(radians(extrinsic[3]))], [0, -sin(radians(extrinsic[3])), cos(radians(extrinsic[3]))]])
         Mph = np.matrix([[cos(radians(extrinsic[4])), 0, -sin(radians(extrinsic[4]))], [0, 1, 0], [sin(radians(extrinsic[4])), 0, cos(radians(extrinsic[4]))]])
         Mkp = np.matrix([[cos(radians(extrinsic[5])), sin(radians(extrinsic[5])), 0], [-sin(radians(extrinsic[5])), cos(radians(extrinsic[5])), 0], [0, 0, 1]])
-        rotMat = (Mkp * Mph * Mom).getA().flatten()
-        self.affineMatrix=np.concatenate((rotMat[0:3],[extrinsic[0]],rotMat[3:7],[extrinsic[1]],rotMat[7:9],[extrinsic[2]],[0],[0],[0],[1]))
+        rotMat = (Mkp * Mph * Mom).T.getA().flatten()
+        self.affineMatrix=np.concatenate((rotMat[0:3],[extrinsic[0]],rotMat[3:6],[extrinsic[1]],rotMat[6:9],[extrinsic[2]],[0],[0],[0],[1]))
         self.affineMatrixString = ' '.join([str(elem) for elem in self.affineMatrix])
 
 
@@ -39,7 +39,7 @@ class SnowyOwl():
         '''
         listtmpfiles = os.listdir(self.outfolder + "tmp/")
         for f in range(0,len(listtmpfiles)):
-            opl.convertBin2LAS(self.outfolder + "tmp/" + listtmpfiles[f], deleteBin=True)
+            opl.convertBin2LAS(self.outfolder + "tmp/" + listtmpfiles[f], deleteBin=False)
             # Tranform cloud accoring to extrinsics
             # create pdal transormation JSON
             json = """
@@ -51,7 +51,7 @@ class SnowyOwl():
                 },
                 {
                     "type":"writers.las",
-                    "filename":""" + "\"" + self.outfolder + "tmp/" +  listtmpfiles[f] + """_transformed.las"
+                    "filename":""" + "\"" + self.outfolder +  listtmpfiles[f] + """_transformed.las"
                 }
             ]
             """
