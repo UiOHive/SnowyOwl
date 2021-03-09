9d32ff3a7367087214849d8fe080c31077730056
Luc <luc.girod@gmai.com>
2021-03-09 14:27:08 +0100

    typo

--- app/snowyowl/SnowyOwl_exampleAcquisition.py
+++ app/snowyowl/SnowyOwl_exampleAcquisition.py
@@ -3,5 +3,5 @@ from snowyowl import SnowyOwlAcquisition as SOA
 path = "/home/pi/data/livox_raw/"
 testA=SOA.SnowyOwlAcquisition(outfolder=path, ip_livox="192.168.1.104", ip_computer="192.168.1.2")
 
-test.acquiereNCloud(number_of_scans=4,duration_between_scans=0, duration=10)
+testA.acquiereNCloud(number_of_scans=4,duration_between_scans=0, duration=10)
 
