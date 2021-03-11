ed8693cad0a3009886792d7b5409286fdcad9ac3
Luc <luc.girod@gmai.com>
2021-03-11 17:51:41 +0100

    Updated ip_livox

--- app/run_files/SnowyOwlAquisition_Finse.py
+++ app/run_files/SnowyOwlAquisition_Finse.py
@@ -1,6 +1,6 @@
 from snowyowl import SnowyOwlAcquisition as SOA
 import logging
 
-testA=SOA.SnowyOwlAcquisition(outfolder="/mnt/LivoxDrive/", ip_livox="192.168.1.104", ip_computer="192.168.13.35")
+testA=SOA.SnowyOwlAcquisition(outfolder="/mnt/LivoxDrive/", ip_livox="192.168.13.104", ip_computer="192.168.13.35")
 testA.acquireClouds(number_of_scans=0,duration_between_scans=10, duration=10)
 
