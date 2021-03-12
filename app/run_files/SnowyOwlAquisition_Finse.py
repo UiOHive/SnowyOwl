8869aff13696d450336c5be6d554d47321c33047
luc-girod <luc.girod@gmail.com>
2021-03-12 08:59:22 +0100

    Added service files for laptop

--- app/run_files/SnowyOwlAquisition_Finse.py
+++ app/run_files/SnowyOwlAquisition_Finse.py
@@ -1,6 +1,15 @@
 from snowyowl import SnowyOwlAcquisition as SOA
 import logging
+import getpass
 
-testA=SOA.SnowyOwlAcquisition(outfolder="/mnt/LivoxDrive/", ip_livox="192.168.13.104", ip_computer="192.168.13.35")
+if getpass.getuser()=='livoxpi':
+    ip_computer="192.168.13.35"
+    outfolder="/mnt/LivoxDrive/"
+elif getpass.getuser()=='snowyowl':
+    ip_computer="192.168.13.36"
+    outfolder="/home/snowyowl/data/LivoxDirect/"
+
+
+testA=SOA.SnowyOwlAcquisition(outfolder=outfolder, ip_livox="192.168.1.104", ip_computer=ip_computer)
 testA.acquireClouds(number_of_scans=0,duration_between_scans=10, duration=10)
 
