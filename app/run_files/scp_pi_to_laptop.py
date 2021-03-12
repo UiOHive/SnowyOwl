3862c1b9e39e543e7c5ee32bd5e6c59b2b372283
Luc <luc.girod@gmai.com>
2021-03-12 09:20:52 +0100

    Removed service for laptop (will to crontab instead

--- app/run_files/scp_pi_to_laptop.py
+++
@@ -1,4 +0,0 @@
-from snowyowl import SnowyOwlAcquisition as SOA
-Owl=SOA.SnowyOwlAcquisition(outfolder="/home/snowyowl/data/LIVOX/")
-
-Owl.sendDataToProcessing(server='finseflux.dyndns.org', username='snowyowl', password='finse_girod', remote_path=b'/home/snowyowl/data/LIVOX/bin/')
--- app/systemd_files/SnowyOwlProcessing_Finse.sh
+++
@@ -1,2 +0,0 @@
-conda activate livox_proc
-python /home/snowyowl/git/SnowyOwl/app/run_files/SnowyOwlProcessing_Finse.py
