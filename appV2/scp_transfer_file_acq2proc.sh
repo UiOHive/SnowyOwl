396335c0b56a383d6a73924119b3339a7c1a8f0f
Luc Girod <luc.girod@gmail.com>
2021-03-23 18:07:34 +0100

    Divided the scp files for now

---
+++ appV2/scp_transfer_file_acq2proc.sh
@@ -0,0 +1,42 @@
+# Script to push newly acquired file to snowyowl for further processing
+
+# USER SETTINGS
+CONFIG_FILE=config.ini
+TRANSFER="transfer_acq2proc"
+delete_transfered_file=true
+reboot_system=true
+
+
+# Pull information from config_file
+DATA_FOLDER=$(sed -n "/^\[$TRANSFER\]/ { :l /^send_folder[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./$CONFIG_FILE)
+PCL_FILES=$DATA_FOLDER*
+TARGET_USER=$(sed -n "/^\[$TRANSFER\]/ { :l /^target_ssh_user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./$CONFIG_FILE)
+TARGET_IP=$(sed -n "/^\[$TRANSFER\]/ { :l /^target_IP_address[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./$CONFIG_FILE)
+TARGET_FOLDER=$(sed -n "/^\[$TRANSFER\]/ { :l /^target_folder[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./$CONFIG_FILE)
+ARCHIVE_FOLDER=$(sed -n "/^\[$TRANSFER\]/ { :l /^archive_folder[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./$CONFIG_FILE)
+
+
+cd $DATA_FOLDER
+if [[ "$DATA_FOLDER" == "$(pwd)/" ]];
+then
+for f in $PCL_FILES
+do
+  echo "Processing $f file..."
+  scp $f $TARGET_USER@$TARGET_IP:$TARGET_FOLDER
+  # uncomment if wanna keep all raw data. WARNING check disk capacity for duration
+
+
+  if $delete_transfered_file;
+  then
+    # remove raw file after file has been sent
+    rm $f
+  else
+    mv  $f $ARCHIVE_FOLDER
+  fi
+done
+fi
+
+if $reboot_system;
+then
+ sudo reboot
+fi
