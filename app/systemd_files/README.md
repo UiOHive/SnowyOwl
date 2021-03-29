50d058d529216a3dbbe48cd31bafa09ceddd4ebf
Luc Girod <luc.girod@gmail.com>
2021-03-29 10:16:43 +0200

    Removed service systemd,as it didn't work out

--- app/systemd_files/README.md
+++
@@ -1,3 +0,0 @@
-To activate the services:
-sudo systemctl daemon-reload
-sudo systemctl enable #NAME_OF_SERVICE#.service
--- app/systemd_files/systemdAquisition.sh
+++
@@ -1,1 +0,0 @@
-/home/livoxpi/miniconda3/envs/livoxenv/bin/python /home/livoxpi/git/SnowyOwl/appV2/acquisition.py -cf /home/livoxpi/config.ini
