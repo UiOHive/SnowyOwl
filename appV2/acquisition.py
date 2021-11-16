ca1e7646cafa09afd7dcd34164bc7b48df0a0e53
arcticsnow <sfilhol@protonmail.com>
2021-11-16 10:22:56 +0100

    import reboot_lidar()

--- appV2/acquisition.py
+++ appV2/acquisition.py
@@ -12,7 +12,7 @@ import openpylivox as opl
 from datetime import datetime
 import time, logging
 import configparser
-import reboot_livox
+from reboot_livox import reboot_lidar
 
 def acquire_clouds(scan_duration=3.0,
                    scan_interval=10,
@@ -95,6 +95,6 @@ if __name__ == "__main__":
                        scan_interval=config.getint('acquisition', 'scanning_interval'),
                        nb_scan_max=config.getint('acquisition', 'number_of_scan_max'),
                        folder=config.get('acquisition', 'data_folder'),
-                       IP_sensor=config.get('acquisition', 'scanner_IP'),
+                           IP_sensor=config.get('acquisition', 'scanner_IP'),
                        IP_computer=config.get('acquisition', 'computer_IP'))
-        reboot_livox.reboot_lidar(config)
+        reboot_lidar(config)
