176758a7ee30b251408de3f4486d6d8461eb4d24
arcticsnow <sfilhol@protonmail.com>
2023-04-20 22:41:57 +0200

    review snap webcam script

--- appV3/snap_picture_webcam.py
+++ appV3/snap_picture_webcam.py
@@ -1,7 +1,6 @@
 import cv2
 import datetime
 import argparse, os
-import configparser
 
 def filename_builder():
     today = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
@@ -13,22 +12,28 @@ def snap_picture(fname, cam_param, path='./'):
     url = f"rtsp://{cam_param['cam_user']}:{cam_param['cam_pwd']}@{cam_param['cam_IP']}:{cam_param['cam_port']}/test.mjpg"
     print(url)
     cap = cv2.VideoCapture(url)
-    cv2.imwrite(f'{path}{fname}', cap.read()[1])
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 3840)
+    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 2160)
+
+    fout = f'{path}{fname}'
+    cv2.imwrite(fout, cap.read()[1])
+    print(f'---> Image {fname} stored as {fout}')
+    cap.release()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_file', '-cf', help='Path to config file', default='/home/config.ini')
+    parser.add_argument('--IP_address', '-ip', help='IP of webcam', default='192.168.1.120')
+    parser.add_argument('--port', '-p', help='Port of webcam', default='554')
+    parser.add_argument('--user', '-u', help='User of webcam', default='admin')
+    parser.add_argument('--password', '-pwd', help='Password of webcam', default='*******')
+    parser.add_argument('--output_dir', '-o', help='directory for output image', default='./myfolder/')
     args = parser.parse_args()
 
-    config = configparser.ConfigParser(allow_no_value=True)
-    config.read(args.config_file)
-
-    #try:
     fname = filename_builder()
-    cam_param = {'cam_user':config.get('webcam', 'cam_user'),
-                 'cam_pwd':config.get('webcam', 'cam_pwd'),
-                 'cam_IP':config.get('webcam', 'cam_IP'),
-                 'cam_port':config.get('webcam', 'cam_port')}
+    cam_param = {'cam_user':args.user,
+                 'cam_pwd':args.pwd,
+                 'cam_IP':args.IP_address,
+                 'cam_port':args.port}
 
     snap_picture(fname, cam_param, path=config.get('webcam', 'dir_to_images'))
     #except:
