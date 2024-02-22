db4abe187e5c005ba24a307fc0a3b0189140acc5
arcticsnow <sfilhol@protonmail.com>
2024-02-22 19:38:24 +0100

    added relay Adam 4060 control and acquire improvement

--- appV3/acquisition.py
+++ appV3/acquisition.py
@@ -10,15 +10,17 @@ TODO:
 
 import openpylivox as opl
 from datetime import datetime
-import time, logging, sys
-import configparser
+import time, logging
+import argparse, os
 import numpy as np
 
 import subprocess
 
+
 def ping(host):
     command = ['ping', '-c', '1', host]
     return subprocess.call(command) == 0
+
 
 def acquire_clouds(scan_duration=3.0,
                    folder='/home/data/',
@@ -32,19 +34,20 @@ def acquire_clouds(scan_duration=3.0,
     :return:
     """
     if ping(IP_sensor):
-        nb_scan = 0 # Reset value to 0 so the following logic works
+        nb_scan = 0  # Reset value to 0 so the following logic works
         sensor = opl.openpylivox(True)
-        connected = sensor.connect(IP_computer, IP_sensor,  60001,     50001,      40001)
+        connected = sensor.connect(IP_computer, IP_sensor, 60001, 50001, 40001)
         if sensor._isConnected:
             logging.info("Connection to Livox scanner successful")
             sensor.setExtrinsicToZero()
-            sensor.lidarSpinUp()                # Data Acquisition
-            sensor.setLidarReturnMode(2)        # set lidar return mode (0 = single 1st return, 1 = single strongest return, 2 = dual returns
-            sensor.setIMUdataPush(False)        # activate the IMU data stream (only for Horizon and Tele-15 sensors)
-            sensor.setRainFogSuppression(False) # turn on (True) or off (False) rain/fog suppression on the sensor
+            sensor.lidarSpinUp()  # Data Acquisition
+            sensor.setLidarReturnMode(
+                2)  # set lidar return mode (0 = single 1st return, 1 = single strongest return, 2 = dual returns
+            sensor.setIMUdataPush(False)  # activate the IMU data stream (only for Horizon and Tele-15 sensors)
+            sensor.setRainFogSuppression(False)  # turn on (True) or off (False) rain/fog suppression on the sensor
             # False here because we are interested in catching snow particles moving through the sensor
             print("\n***** Done setting parameters *****\n")
-            filename = folder + "tmp/" + datetime.utcnow().strftime("%Y.%m.%dT%H-%M-%S.bin")
+            filename = folder + datetime.utcnow().strftime("%Y.%m.%dT%H-%M-%S.bin")
             sensor.dataStart_RT_B()  # start data stream (real-time writing of point cloud data to a BINARY file)
             sec_wait = 0  # seconds, time delayed data capture start
             # (*** IMPORTANT: this command starts a new thread, so the current program (thread) needs to exist for the 'duration' ***)
@@ -52,21 +55,21 @@ def acquire_clouds(scan_duration=3.0,
             sensor.saveDataToFile(filename, sec_wait, scan_duration)
             acquisition_start_time = time.perf_counter()
             while True:
-                if sensor.doneCapturing() or ((time.perf_counter()-acquisition_start_time)>(2*scan_duration)):
-                    logging.info("Data capture stopped after {}s".format(time.perf_counter()-acquisition_start_time))
-                    if (time.perf_counter() - acquisition_start_time)<(0.5 * scan_duration):
+                if sensor.doneCapturing() or ((time.perf_counter() - acquisition_start_time) > (2 * scan_duration)):
+                    logging.info("Data capture stopped after {}s".format(time.perf_counter() - acquisition_start_time))
+                    if (time.perf_counter() - acquisition_start_time) < (0.5 * scan_duration):
                         logging.info("Data capture was much shorter than expected, disconnecting and starting again")
-                        #sensor.disconnect()
-                    elif (time.perf_counter() - acquisition_start_time)>(2 * scan_duration):
+                        # sensor.disconnect()
+                    elif (time.perf_counter() - acquisition_start_time) > (2 * scan_duration):
                         logging.info("Data capture was much longer than expected, disconnecting and starting again")
-                        #sensor.disconnect()
+                        # sensor.disconnect()
                     else:
                         sensor.dataStop()
                         logging.info(f"Lidar loop {nb_scan} ==== Cloud acquired with name {filename}")
                     break
 
             sensor.disconnect()
-            #logging.info("Disconnected from LIVOX, reached max nb of scans limit: {}".format(nb_scan_max))
+            # logging.info("Disconnected from LIVOX, reached max nb of scans limit: {}".format(nb_scan_max))
         else:
             print(f"\n***** Could not connect to Livox sensor with IP address {IP_sensor} *****\n")
             logging.error(f"Could not connect to Livox sensor with IP address {IP_sensor}")
@@ -75,9 +78,7 @@ def acquire_clouds(scan_duration=3.0,
         logging.error(f'IP {IP_sensor} does not ping')
 
 
-
 if __name__ == "__main__":
-    import argparse, os
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--scan_duration', '-d', help='Scan duration (s)', default=10)
@@ -90,7 +91,7 @@ if __name__ == "__main__":
     os.makedirs(path_to_data, exist_ok=True)
     os.makedirs(path_to_data + 'bins', exist_ok=True)
     os.makedirs(path_to_data + 'archive', exist_ok=True)
-    
+
     logging.basicConfig(filename=path_to_data + 'lidar_acquisition.log',
                         level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s : %(message)s')
@@ -99,6 +100,6 @@ if __name__ == "__main__":
     TODO HERE: add logic to check if folder structure is good and existing
     '''
     acquire_clouds(scan_duration=np.int64(args.scan_duration),
-                       folder=args.output_folder,
-                       IP_sensor=args.IP_scanner,
-                       IP_computer=args.IP_computer)
+                   folder=args.output_folder,
+                   IP_sensor=args.IP_scanner,
+                   IP_computer=args.IP_computer)
